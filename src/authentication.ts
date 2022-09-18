8cd206cf90f3df1cd7a7cd5ed86bc97cc56bf77f
ooatamelbug <mohamedwork636@gmail.com>
2022-09-19 01:58:50 +0200

    add Facebook auth

--- src/authentication.ts
+++ src/authentication.ts
@@ -1,9 +1,11 @@
-import { ServiceAddons } from '@feathersjs/feathers';
-import { AuthenticationService, JWTStrategy } from '@feathersjs/authentication';
+import { Params, ServiceAddons } from '@feathersjs/feathers';
+import { AuthenticationRequest, AuthenticationService, JWTStrategy } from '@feathersjs/authentication';
 import { LocalStrategy } from '@feathersjs/authentication-local';
-import { expressOauth, OAuthStrategy } from '@feathersjs/authentication-oauth';
+import { expressOauth, OAuthProfile, OAuthStrategy } from '@feathersjs/authentication-oauth';
 
 import { Application } from './declarations';
+
+import axios from 'axios';
 
 declare module './declarations' {
   interface ServiceTypes {
@@ -11,9 +13,33 @@ declare module './declarations' {
   }
 }
 
+class FacebookStrategy extends OAuthStrategy {
+  async getProfile(authResult: AuthenticationRequest, _param: Params) {
+    const accessToken = authResult.access_token;
+    const { data } = await axios.get('https://graph.facebook.com/me', {
+      headers: {
+        authorication: `bearer ${accessToken}`,
+      },
+      params: {
+        field: 'id,name,email,picture'
+      }
+    });
+    return data;
+  }
+
+  async getEntityData(profile: OAuthProfile, existing: any, param: Params) {
+    const baseData = await super.getEntityData(profile, existing, param);
+    return {
+      ...baseData,
+      email: profile.email,
+      image: profile.picture, 
+    };
+  }
+}
+
 class GoogleStrategy extends OAuthStrategy {
-  async getEntityData(profile: any) {
-    const baseData = await super.getEntityData(profile, false, { });
+  async getEntityData(profile: OAuthProfile,  existing: any, param: Params) {
+    const baseData = await super.getEntityData(profile, existing, param);
 
     return {
       ...baseData,
@@ -29,6 +55,7 @@ export default function(app: Application): void {
   authentication.register('jwt', new JWTStrategy());
   authentication.register('local', new LocalStrategy());
   authentication.register('google', new GoogleStrategy());
+  authentication.register('facebook', new FacebookStrategy());
 
   app.use('/authentication', authentication);
   app.configure(expressOauth());
